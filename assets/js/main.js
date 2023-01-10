9b332f9f49a262d3732b7019ad9cda3f62005cd4
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-10 18:15:27 +0100

    hamburger_menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
- 
+let hamburgeMenu = document.getElementById("hamburgerMenu");
+let hamburgerNav = document.getElementById("hamburgerNav");
+
+let clickCounter = 0;
+
+let counter = () => {
+    clickCounter ++;
+    console.log(clickCounter);
+    if(clickCounter % 2 !== 0){
+        hamburgerNav.style.display="block";
+    }
+    else{
+        hamburgerNav.style.display="none";
+    }
+}
